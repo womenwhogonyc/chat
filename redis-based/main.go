ae5db39bc59f00f776d1002b321f61566254d705
Kevin Cantwell <kevin.cantwell@gmail.com>
2015-09-20 14:25:11 -0400

    basic server implemented

--- redis-based/main.go
+++ redis-based/main.go
@@ -3,15 +3,31 @@ package main
 import (
 	"fmt"
 	"log"
+	"net/http"
 
+	"github.com/gorilla/mux"
 	"github.com/timehop/jimmy/redis"
 )
 
+var Redis redis.Pool
+
 func main() {
-	db, err := redis.NewPool("redis://localhost:6379", redis.DefaultConfig)
+	var err error
+	Redis, err = redis.NewPool("redis://localhost:6379", redis.DefaultConfig)
 	if err != nil {
 		log.Fatal(err.Error())
 	}
-	foo, err := db.Get("foo")
-	fmt.Println(foo, err)
+
+	router := mux.NewRouter()
+	router.HandleFunc("/chat/{room_id}", HandleChatRoom)
+
+	http.Handle("/", router)
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err.Error())
+	}
+}
+
+func HandleChatRoom(w http.ResponseWriter, r *http.Request) {
+	roomID := mux.Vars(r)["room_id"]
+	w.Write([]byte(fmt.Sprintf("Chat room %s is coming Soon!", roomID)))
 }
