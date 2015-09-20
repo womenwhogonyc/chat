1219259725504cd9badf93b182f857c305afc0c7
Kevin Cantwell <kevin.cantwell@gmail.com>
2015-09-20 14:15:46 -0400

    use github.com/timehopjimmy/redis

--- redis-based/main.go
+++ redis-based/main.go
@@ -1,5 +1,17 @@
 package main
 
+import (
+	"fmt"
+	"log"
+
+	"github.com/timehop/jimmy/redis"
+)
+
 func main() {
-	// TODO
+	db, err := redis.NewPool("redis://localhost:6379", redis.DefaultConfig)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	foo, err := db.Get("foo")
+	fmt.Println(foo, err)
 }
